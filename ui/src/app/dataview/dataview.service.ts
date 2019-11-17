f6183f096cbcae9571dfddb8325277e57be8cbf9
Abhilasha Singh <singh.abhilasha@tcs.com>
2019-11-16 22:32:15 -0500

    Upload style fix

--- ui/src/app/dataview/dataview.service.ts
+++ ui/src/app/dataview/dataview.service.ts
@@ -1,16 +1,22 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
+import { Options } from 'selenium-webdriver/chrome';
 
 @Injectable()
 
 export class DataviewService {
+    
     constructor(private http: HttpClient) { }
 
 
 
     loadData (){
         return this.http.get<any>("http://127.0.0.1:5000/series");
+    }
+
+    loadSummaryData (){
+        return this.http.get<any>("http://127.0.0.1:5000/arimaseries");
     }
 
   }
